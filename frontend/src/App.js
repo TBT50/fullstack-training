1fd7081725bdae8bdcc0eb0d6a83ea28d5a10c39
PiotrK <piotrek380@gmail.com>
2021-11-17 21:05:48 +0100

    Add functionality to add notes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
+import { useState } from "react";
 
-import notes from "./notes";
+import notesList from "./notes";
 
 import { Note } from "./components/Note";
 
 function App() {
+  const [notes, setNotes] = useState(notesList);
+  const [newNote, setNewNote] = useState("");
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const newObj = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    };
+    setNotes([...notes, newObj]);
+    setNewNote("");
+  };
+
+  const handleChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
   return (
     <div className="App">
       <h1>Notes</h1>
@@ -18,6 +38,19 @@ function App() {
           />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <div>
+          <label htmlFor="newNote">New note</label>
+          <input
+            type="text"
+            value={newNote}
+            onChange={handleChange}
+            id="newNote"
+            name="newNote"
+          />
+        </div>
+        <button type="submit">Add Note</button>
+      </form>
     </div>
   );
 }
