6744d43ed6d5cd85843596b877ae8f94d2c294a5
PiotrK <piotrek380@gmail.com>
2022-04-16 20:09:56 +0200

    Delete all files and directories

--- frontend/src/components/Note.js
+++
@@ -1,3 +0,0 @@
-export const Note = ({ content, date, important }) => {
-  return <li>{content}</li>;
-};
--- frontend/src/routes/expenses.js
+++
@@ -1,7 +0,0 @@
-export default function Expenses() {
-  return (
-    <main>
-      <h1>Expenses</h1>
-    </main>
-  );
-}
